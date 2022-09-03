6e42a2940a75ddd45436d53a903a82b24e94e3a9
Abhaypokhriyal <112252244+Abhaypokhriyal@users.noreply.github.com>
2022-09-03 08:42:23 +0530

    Add files via upload
    uploaded

---
+++ app/mock-employee-data.ts
@@ -0,0 +1,30 @@
+import { Employee } from "./Employee";
+
+export class EmployeeMockData{
+
+    public static employeeList:Employee[] = [
+        
+        {
+            'employeeId' :123,
+            'employeeName': 'Hair Oil',
+            'employeeMobileNo' :   '121',
+            'employeeEmail' : 'Cosmectics',
+            'employeeSalary' :   20,
+        },
+        {
+             'employeeId' :124,
+            'employeeName': 'Comb',
+            'employeeMobileNo' :   '122',
+            'employeeEmail' : 'Household',
+            'employeeSalary' :   30,
+        },
+        {
+            'employeeId' :125,
+            'employeeName': 'Face wash',
+            'employeeMobileNo' :   '123',
+            'employeeEmail' : 'Cosmectics',
+            'employeeSalary' :   50,
+        }
+    ];
+
+}
